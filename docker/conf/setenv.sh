7f634f20a1ecb0df0692c6cacf4aea5b30f21e26
Roderick Constance <justaddrobots@icloud.com>
2022-11-02 22:56:30 -0700

    ISSUE-004: Added ansible and docker bits for build

---
+++ docker/conf/setenv.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.16.1.1-1.el8_6.x86_64
+export TEAMCITY_DATA_PATH=/opt/jetbrains/TeamCity/data
+export TEAMCITY_LOGS_PATH=/opt/jetbrains/TeamCity/logs
+export TEAMCITY_SERVER_MEM_OPTS=-Xmx2048m
