52c7d82f70cacf70de39ed351af9215b4d152849
Roderick Constance <justaddrobots@icloud.com>
2022-11-09 00:21:02 -0800

    ISSUE-004: Added MySQL config updates

--- docker/conf/setenv.sh
+++ docker/conf/setenv.sh
@@ -4,3 +4,9 @@ export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.16.1.1-1.el8_6.x86_64
 export TEAMCITY_DATA_PATH=/opt/jetbrains/TeamCity/data
 export TEAMCITY_LOGS_PATH=/opt/jetbrains/TeamCity/logs
 export TEAMCITY_SERVER_MEM_OPTS=-Xmx2048m
+export MYSQL_ROOT_PASSWORD=Ch4n63M3
+export MYSQL_ADMIN_USER=rcon
+export MYSQL_ADMIN_PASSWORD=Sharapova
+export MYSQL_TEAMCITY_USER=teamcity
+export MYSQL_TEAMCITY_PASSWORD=teamcity
+
