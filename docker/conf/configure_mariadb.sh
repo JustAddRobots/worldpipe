3320c00eb60d71fbd04e8a0f003ded77a4d8803d
Roderick Constance <justaddrobots@icloud.com>
2022-11-10 18:04:12 -0800

    ISSUE-004: Added MariaDB, env config updates

--- docker/conf/configure_mariadb.sh
+++ docker/conf/configure_mariadb.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
-# Remove anonymous user
+echo "Remove anonymous user..."
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DROP USER IF EXISTS ''@'localhost'"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DROP USER IF EXISTS ''@'$(hostname)'"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DROP USER IF EXISTS ''@'$(hostname).local'"
 
-# Remove test database
+echo "Remove test database..."
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS test"
 
-# Create admin user
+echo "Create admin user..."
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_ADMIN_USER'@'localhost' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD'"
 
-# Create teamcity user
+echo "Create teamcity user..."
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_TEAMCITY_USER'@'localhost' IDENTIFIED BY '$MYSQL_TEAMCITY_PASSWORD'"
 
-# Create teamcity database
+echo "Create teamcity database..."
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS teamcity COLLATE utf8mb4_bin"
 
-# Grant privileges
+echo "Grant privileges..."
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON teamcity.* TO '$MYSQL_TEAMCITY_USER'@'localhost'"
+
+echo "Flush privileges..."
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES"
--- docker/conf/setenv.sh
+++ docker/conf/setenv.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.16.1.1-1.el8_6.x86_64
+export JRE_HOME=/usr/lib/jvm/java-11-openjdk-11.0.16.1.1-1.el8_6.x86_64
+export TEAMCITY_JRE=/usr/lib/jvm/java-11-openjdk-11.0.16.1.1-1.el8_6.x86_64
 export TEAMCITY_DATA_PATH=/opt/jetbrains/TeamCity/data
 export TEAMCITY_LOGS_PATH=/opt/jetbrains/TeamCity/logs
 export TEAMCITY_SERVER_MEM_OPTS=-Xmx2048m
@@ -9,4 +11,3 @@ export MYSQL_ADMIN_USER=rcon
 export MYSQL_ADMIN_PASSWORD=Sharapova
 export MYSQL_TEAMCITY_USER=teamcity
 export MYSQL_TEAMCITY_PASSWORD=teamcity
-
